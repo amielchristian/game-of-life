1bd2b3f2fdf625cda592d502c1f2820c2692dd24
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-04-30 05:48:03 +0800

    Remove pkg from .gitignore

--- game-of-life-web/vite.config.ts
+++ game-of-life-web/vite.config.ts
@@ -5,6 +5,6 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/game-of-life/',
   plugins: [preact(), wasm(), topLevelAwait()],
 });
